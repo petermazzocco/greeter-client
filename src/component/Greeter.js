88b6543fca1c195ab45062d70352274731066ac1
PM <100664895+OhItsPM@users.noreply.github.com>
2023-02-24 14:27:38 -0800

    Fixed errors

--- src/component/Greeter.js
+++ src/component/Greeter.js
@@ -2,29 +2,23 @@ import { ethers } from "ethers";
 import { useState, useEffect } from "react";
 import { abi } from "../utils/greeterABI";
 import globe from "../img/globe.gif";
+import Metamask from "./Metamask";
 
 const greeterContractABI = abi;
 const greeterAddress = "0xfa81A6e1f8651E7564273BB3a97C2B3d4E40d075";
 const Greeter = () => {
   const [currentGreeting, setCurrentGreeting] = useState("");
   const [newGreeting, setNewGreeting] = useState("");
-  const [txHash, setTxHash] = useState("");
+  // const [txHash, setTxHash] = useState("");
 
   //Get current greeting
   useEffect(() => {
     const loadCurrentGreeting = async () => {
-      const provider = new ethers.AlchemyProvider(
-        "goerli",
-        process.env.REACT_APP_API_KEY
-      );
-      const wallet = new ethers.Wallet(
-        process.env.REACT_APP_PRIVATE_KEY,
-        provider
-      );
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
       const greeterContract = new ethers.Contract(
         greeterAddress,
         greeterContractABI,
-        wallet
+        provider
       );
 
       const currentGreeting = await greeterContract.greet();
@@ -40,10 +34,7 @@ const Greeter = () => {
 
   //New greeter message
   const handleSetGreeting = async () => {
-    const provider = new ethers.AlchemyProvider(
-      "goerli",
-      process.env.REACT_APP_API_KEY
-    );
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     const greeterContract = new ethers.Contract(
       greeterAddress,
@@ -51,9 +42,9 @@ const Greeter = () => {
       signer
     );
 
-    const setGreetingTx = await greeterContract.setGreeting(newGreeting);
-    // console.log(`Greeting transaction: ${setGreetingTx.hash}`);
-    setTxHash(setGreetingTx);
+    // const setGreetingTx = await greeterContract.setGreeting(newGreeting);
+    // // console.log(`Greeting transaction: ${setGreetingTx.hash}`);
+    // // setTxHash(setGreetingTx);
 
     // Update current greeting
     const updatedGreeting = await greeterContract.greet();
@@ -72,8 +63,8 @@ const Greeter = () => {
       <p className="text-xl font-thin">
         Create a new greeter message that is verifiable on the Goerli tesnet!
       </p>
-      <div className="pb-10">
-        <p>Current greeting: {currentGreeting}</p>
+      <div className="pb-2 flex flex-col">
+        <p>Current greeting: </p> <p className="font-thin">{currentGreeting}</p>
       </div>
       <label className="pb-2">
         Enter A New Greeting:
@@ -85,20 +76,13 @@ const Greeter = () => {
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         />
       </label>
+      <Metamask />
       <button
         onClick={handleSetGreeting}
         className="text-gray-600 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
       >
         Set greeting
       </button>
-
-      {!txHash ? (
-        <div className="grid justify-center">
-          <p></p>
-        </div>
-      ) : (
-        <p>View the transaction hash: {txHash}</p>
-      )}
     </div>
   );
 };
--- src/utils/greeterABI.js
+++ src/utils/greeterABI.js
@@ -1,25 +1,21 @@
 export const abi = [
-  [
-    {
-      inputs: [{ internalType: "string", name: "_greeting", type: "string" }],
-      stateMutability: "nonpayable",
-      type: "constructor",
-    },
-    {
-      inputs: [],
-      name: "greet",
-      outputs: [{ internalType: "string", name: "", type: "string" }],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [
-        { internalType: "string", name: "_newGreeting", type: "string" },
-      ],
-      name: "setGreeting",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-  ],
+  {
+    inputs: [{ internalType: "string", name: "_greeting", type: "string" }],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "greet",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "string", name: "_newGreeting", type: "string" }],
+    name: "setGreeting",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
 ];
